f633af231464f2995c6f283ef1c281f27e9ae8f7
cyber-meow <sjungle305@gmail.com>
2017-06-12 04:40:03 +0800

    some more patterns

--- paper/codes/patterns.py
+++ paper/codes/patterns.py
@@ -13,20 +13,28 @@ def pattern(func):
     return new_func
 
 @pattern
-def triangles(T, mag, t):
+def triangles(T, amp, t):
     n = t // T
-    y_delta = (t-T*n) / T * 2 * mag
+    y_delta = (t-T*n) / T * 2 * amp
     if n % 2 == 0:
         return mag-y_delta
     return -mag+y_delta
 
 @pattern
-def four_sin(T, t):
-    s1 = np.sin(2*np.pi*t/T)
-    s2 = np.sin(2*np.pi*t/T+np.pi/2)
-    s3 = np.sin(2*np.pi*t/T+np.pi)
-    s4 = np.sin(2*np.pi*t/T+np.pi*3/2)
+def four_sins(T, amp, t):
+    s1 = amp * np.sin(2*np.pi*t/T)
+    s2 = amp / 2 * np.sin(4*np.pi*t/T)
+    s3 = amp / 6 * np.sin(6*np.pi*t/T)
+    s4 = amp / 3 * np.sin(8*np.pi*t/T)
     return s1+s2+s3+s4
+
+@pattern
+def sixteen_sins(T, amp, t):
+    ret = 0
+    amp_divs = [1, 2, 6, 3, 9, 12, 4, 3, 8, 16, 10, 5, 4, 6, 2, 8]
+    for i in range(16):
+        ret += amp / amp_divs[i] * np.sin(2*i*np.pi*t/T)
+    return ret
     
 def plot_patterns(T, p):
     ts = np.arange(0,T,1e-3)
