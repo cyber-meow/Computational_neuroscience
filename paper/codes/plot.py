eb68f4a282af518d84a8d5c721e92e47e5e633ad
unknown <sjungle305@gmail.com>
2017-06-17 23:54:03 +0900

    continue to add stuffs

--- paper/codes/plot.py
+++ paper/codes/plot.py
@@ -30,12 +30,17 @@ def plot_rs(force, neurons):
         ax.axes.get_yaxis().set_visible(False)
     add_bottom_line(fig, axs[-1])
     
-def compare_generate(force, T, pic_name=None):
+def compare_generate(force, T, pic_name=None, sep=False):
     force.init_exp("new")
-    force.simulate(T, exp_name="new", update=False)
-    fig, ax = plt.subplots()
+    force.simulate(T, exp_name="new", update=False)    
     exp = force.exps["new"]
-    exp.plot_fs(force.f, ax)
+    if sep:
+        fig, ax = plt.subplots(figsize=(8,12))
+        dec = np.ptp(exp.z_his)*1.2
+        exp.plot_fs(lambda t: force.f(t)-dec, ax)
+    else:
+        fig, ax = plt.subplots()
+        exp.plot_fs(force.f, ax)
     exp.plot_zs(ax)
     ax.set_xlabel('time $t$ (s)')
     ax.axes.get_yaxis().set_visible(False)
@@ -49,23 +54,39 @@ def exp_PCA(exp):
     r_pca = pca.fit_transform(exp.r_his)
     return pca, r_pca
 
-def plot_rs_pca(r_pca):
+def plot_rs_pca(exp, r_pca):
     rs = np.transpose(r_pca[:,:8])
-    fig, axs = plt.subplots(8, sharex=True, facecolor='white')
-    for i,ax in enumerate(axs):
-        ax.plot(rs[i], color='darkgoldenrod')
-        hide_ax(ax)
+    fig, ax = plt.subplots(facecolor='white')
+    offset = 0
+    for r in rs[::-1]:
+        ax.plot(exp.from0(exp.t_his), r+offset, color='darkgoldenrod')
+        offset += 1.3*np.ptp(r)
+    ax.set_frame_on(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.margins(0.03, 0.05)    
+    add_bottom_line(fig, ax)
+    ax.set_xlabel("time $t$ (s)")
 
 def plot_zs_pca(force, exp, pca, r_pca):
-    fig, (ax1, ax2) = plt.subplots(2, sharex=True, facecolor='white')
-    exp.plot_zs(ax1)
-    hide_ax(ax1)
+    fig, ax= plt.subplots(facecolor='white')
+    exp.plot_zs(ax)
     pca8 = np.zeros_like(r_pca)
     pca8[:,:8] = r_pca[:,:8]
     rs_pca8 = pca.inverse_transform(pca8)
     zs_pca8 = np.dot(rs_pca8, force.w)
-    ax2.plot(exp.t_his, zs_pca8, color='darkgoldenrod')
-    hide_ax(ax2)
+    oft = 1.25*np.ptp(exp.z_his)
+    ax.plot(exp.from0(exp.t_his), zs_pca8-oft, color='darkgoldenrod')
+    ax.set_frame_on(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ymin, ymax = ax.get_yaxis().get_view_interval()
+    ax.set_ylim(ymin-0.3)
+    add_bottom_line(fig, ax)
+    ax.set_xlabel("time $t$ (s)")
 
 def plot_eigenvalues(pca):
-    plt.plot(pca.explained_variance_)
+    fig, ax = plt.subplots()
+    ax.plot(pca.explained_variance_)
+    ax.set_yscale('log')
+    ax.axhline(1, color='black')
+    ax.set_ylabel("eigenvalue")
+    ax.set_xlabel("principal components")
